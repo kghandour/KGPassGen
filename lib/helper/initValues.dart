f37510c390a2e8b56a37023a35b64fb1aa381650
Karim ElGhandour <kghandour@live.com>
2022-09-09 23:23:04 +0200

    Cleaned up initializing configuration

---
+++ lib/helper/initValues.dart
@@ -0,0 +1,31 @@
+import 'dart:developer';
+
+import 'package:kg_passgen/controller/configuration_controller.dart';
+import 'package:kg_passgen/controller/general_controller.dart';
+import 'package:kg_passgen/model/general.dart';
+
+import '../model/configuration.dart';
+
+List initializeGeneralConfig(configurationBox, generalBox) {
+  final configurations = configurationBox.values.toList().cast<Configuration>();
+  final generalOptions = generalBox.values.toList().cast<General>();
+
+  Configuration config;
+  General general;
+
+  if (configurations.length == 0) {
+    config = Configuration();
+    ConfigurationController.addConfiguration(config, configurationBox);
+  } else {
+    config = configurations.first;
+  }
+
+  if (generalOptions.length == 0) {
+    general = General();
+    GeneralController.addGeneral(general, generalBox);
+  } else {
+    general = generalOptions.first;
+  }
+
+  return [configurations, generalOptions, config, general];
+}
--- lib/presentation/pages/configuration_page.dart
+++ lib/presentation/pages/configuration_page.dart
@@ -1,12 +1,10 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:kg_passgen/controller/boxes.dart';
-import 'package:kg_passgen/controller/configuration_controller.dart';
-import 'package:kg_passgen/controller/general_controller.dart';
+import 'package:kg_passgen/helper/initValues.dart';
 import 'package:kg_passgen/model/configuration.dart';
 import 'package:kg_passgen/model/general.dart';
+import 'package:kg_passgen/presentation/widgets/MultiValueListenableBuilder.dart';
 
 class ConfigurationPage extends StatefulWidget {
   @override
@@ -18,59 +16,40 @@ class _ConfigurationPageState extends State<ConfigurationPage> {
 
   @override
   Widget build(BuildContext context) {
-    return ValueListenableBuilder<Box<Configuration>>(
-        valueListenable: Boxes.getConfigurations().listenable(),
-        builder: (context, configurationBox, _) {
-          final configurations =
-              configurationBox.values.toList().cast<Configuration>();
+    return MultiValueListenableBuilder(
+      first: Boxes.getGeneral().listenable(),
+      second: Boxes.getConfigurations().listenable(),
+      builder: (context, generalBox, configurationBox, _) {
+        List inits = initializeGeneralConfig(configurationBox, generalBox);
+        final configurations = inits[0];
+        final generalSettings = inits[1];
+        final config = inits[2];
+        final general = inits[3];
 
-          Configuration config;
-          if (configurations.length == 0) {
-            log("Empty list, creating configuration");
-            config = Configuration();
-            ConfigurationController.addConfiguration(config, configurationBox);
-          } else {
-            config = configurations.first;
-          }
-
-          return ValueListenableBuilder<Box<General>>(
-              valueListenable: Boxes.getGeneral().listenable(),
-              builder: (context, generalBox, _) {
-                General general;
-                final generalOptions =
-                    generalBox.values.toList().cast<General>();
-                if (generalOptions.length == 0) {
-                  log("Empty list, creating configuration");
-                  general = General();
-                  GeneralController.addGeneral(general, generalBox);
-                } else {
-                  config = configurations.first;
-                  general = generalOptions.first;
-                }
-                return Column(
-                  children: [
-                    Text(config.name),
-                    Text(config.createdDate.toString()),
-                    Text(config.pwLength.toString()),
-                    Text(config.hashingAlgorithm.toString()),
-                    Text("Show Guide" + general.showGuide.toString()),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.of(context).pushReplacementNamed('/home');
-                      },
-                      style: TextButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(5)),
-                          foregroundColor: Colors.white,
-                          backgroundColor: Colors.red.shade400,
-                          minimumSize: const Size.fromHeight(120)),
-                      child: Text(
-                        'Home',
-                      ),
-                    )
-                  ],
-                );
-              });
-        });
+        return Column(
+          children: [
+            Text(config.name),
+            Text(config.createdDate.toString()),
+            Text(config.pwLength.toString()),
+            Text(config.hashingAlgorithm.toString()),
+            Text("Show Guide" + general.showGuide.toString()),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pushReplacementNamed('/home');
+              },
+              style: TextButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5)),
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.red.shade400,
+                  minimumSize: const Size.fromHeight(120)),
+              child: Text(
+                'Home',
+              ),
+            )
+          ],
+        );
+      },
+    );
   }
 }
