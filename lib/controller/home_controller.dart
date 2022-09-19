9495ae792daeb8974afcf156eaeeb6948bc81aba
Karim ElGhandour <kghandour@live.com>
2022-09-19 22:58:45 +0200

    translated copy message

--- lib/controller/home_controller.dart
+++ lib/controller/home_controller.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class HomePageController {
   static void copyPassword(String _generatedPassword, BuildContext context) {
     Clipboard.setData(ClipboardData(text: _generatedPassword));
-    ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Generated Password copied to clipboard")));
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(AppLocalizations.of(context)!.copyPasswordMessage)));
   }
 }
